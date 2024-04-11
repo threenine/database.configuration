a1d75f2739bcf7bb1a9759b820041f9390d63905
Gary Woodfine <lnb0l9dc@duck.com>
2024-04-11 12:24:58 +0100

    Refactor ModelBuilderExtensions to update table names
    The method "ConfigureOwnedTypeColumnNames" has been renamed and refactored to "ConfigureOwnedTypeTableName". The updated method now changes the

--- src/Extensions/ModelBuilderExtensions.cs
+++ src/Extensions/ModelBuilderExtensions.cs
@@ -6,23 +6,14 @@ namespace Threenine.Database.Extensions;
 
 public static class ModelBuilderExtensions
 {
-    public static void ConfigureOwnedTypeColumnNames(this ModelBuilder modelBuilder)
+    public static void ConfigureOwnedTypeTableName(this ModelBuilder modelBuilder)
     {
         foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
             if (!entityType.IsOwned()) continue;
-           
-            var ownership = entityType.FindOwnership();
-
-            if (ownership is null) continue;
-
-            var properties = entityType.GetProperties().Where(x => !x.IsShadowProperty());
-            
-            foreach (var property in properties)
-            {
-                var tableName = entityType.GetTableName();
-                entityType.SetTableName(tableName.ToSnakeCase());
-            }
+            var tableName = entityType.GetTableName();
+            entityType.SetTableName(tableName.ToSnakeCase());
+          
         }
     }
 }
